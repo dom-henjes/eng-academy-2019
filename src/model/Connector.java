b902a2489cdfdfbe62eae42595025d114dfd320e
Dom Henjes <dominikh@kainos.com>
2019-08-13 06:54:09 +0100

    Expanded DB connection class

--- src/model/Connector.java
+++ src/model/Connector.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
@@ -12,10 +17,30 @@ public class Connector {
 	}
 	
 	private Jdbi getConnector() {
-		if (conn != null) {
-			Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost/world", "worlduser", "worldPwd!");
-			jdbi.installPlugin(new SqlObjectPlugin());
-			conn = jdbi;
+		if (conn == null) {
+			try {
+				FileInputStream propsStream = new FileInputStream("employeesdb.properties");
+				
+				Properties props = new Properties();
+				props.load(propsStream);
+
+				String user = props.getProperty("user");
+				String password = props.getProperty("password");
+				String host = props.getProperty("host");
+				
+				if (user == null || password == null || host == null) {
+					throw new IOException();
+				}
+				
+				Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost/world", "worlduser", "worldPwd!");
+				jdbi.installPlugin(new SqlObjectPlugin());
+				conn = jdbi;
+			} catch (FileNotFoundException e) {
+				System.err.println("Failed to find config file for DB connection.");
+			} catch (IOException e) {
+				System.err.println("Config file for DB connection could not be loaded properly.");
+			}
+			
 		}
 		return conn;
 	}
