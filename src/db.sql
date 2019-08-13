e4f322b49ce2a6994fcddc39de3c2c1afe4de2cc
Dom Henjes <dominikh@kainos.com>
2019-08-13 07:47:49 +0100

    Moved SQL commands to .sql file

--- src/db.sql
+++ src/db.sql
@@ -1,14 +1,26 @@
+DROP DATABASE IF EXISTS jarcompany;
+
 CREATE database jarcompany;
 
 USE jarcompany;
 
 
 CREATE USER 'jaruser'@'localhost' IDENTIFIED by 'password';
-GRANT select on jarcompany.* TO jaruser@localhost;
+GRANT select ON jarcompany.* TO jaruser@localhost;
 
-create table Employee( Name varchar(45) NOT NULL, Address varchar(150) NOT NULL, Email varchar(150) NOT NULL, NI char(9) NOT NULL, accountNumber char(8) NOT NULL, sortCode char(6) NOT NULL, startingSalary double(8,2) NOT NULL, employeeNumber int PRIMARY KEY AUTO_INCREMENT);
+CREATE TABLE Employee(
+employeeNumber int PRIMARY KEY AUTO_INCREMENT,
+Name varchar(45) NOT NULL,
+Address varchar(150) NOT NULL,
+Email varchar(150) NOT NULL, 
+NI char(9) NOT NULL, 
+accountNumber char(8) NOT NULL, 
+sortCode char(6) NOT NULL, 
+startingSalary double(8,2) NOT NULL
+);
 
-INSERT INTO Employee (Name,Address,Email,NI,accountNumber,sortCode,startingSalary) VALUES ('Bruce Wayne','Boston','rnd@kainos.com','12345678','SS12345C','42060','50000');
+INSERT INTO Employee (Name,Address,Email,NI,accountNumber,sortCode,startingSalary)
+VALUES ('Bruce Wayne','Boston','rnd@kainos.com','12345678','SS12345C','42060','50000');
 
 ALTER TABLE Employee
 ADD COLUMN Department varchar(20) NULL;
