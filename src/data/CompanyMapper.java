7e76b720c779f2a3cbe49b18fc877206ae1f259a
Jennifer Myers <jenniferm@kainos.com>
2019-08-15 11:29:53 +0100

    query department added to company mapper

--- src/data/CompanyMapper.java
+++ src/data/CompanyMapper.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.List;
+
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
@@ -8,12 +10,16 @@ import model.Employee;
 
 public interface CompanyMapper {
 
-	@SqlQuery("SELECT Name from Employee where Name = ?")
+	@SqlQuery("SELECT Name FROM Employee WHERE Name = ?")
 	@RegisterBeanMapper(Employee.class)
 	Employee getEmployee(String codename);
 	
+	@SqlQuery("SELECT employeeNumber FROM Employee WHERE Department = ?")
+	@RegisterBeanMapper(Employee.class)
+	List<Employee> getEmployeeByDepartment(String Department);
+	
 	@SqlUpdate("INSERT INTO Employee (Name, Address, Email, NI, accountNumber, sortCode, startingSalary)" +
-	"values (?, ?, ?, ?, ?, ?, ?)")
+	"VALUES (?, ?, ?, ?, ?, ?, ?)")
 	@RegisterBeanMapper(Employee.class)
 	void insertEmployee(String name, String address, String email, String NI, String bankAccount, 
 			float startingSalary);
