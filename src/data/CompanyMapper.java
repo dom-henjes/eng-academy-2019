cb3dcd9a4e2d2b4ef7c33e6bd4be1e7e644c686a
Jennifer Myers <jenniferm@kainos.com>
2019-08-15 11:15:14 +0100

    insert values added to company mapper for employee

--- src/data/CompanyMapper.java
+++ src/data/CompanyMapper.java
@@ -2,12 +2,21 @@ package data;
 
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import model.Employee;
 
 public interface CompanyMapper {
-	
-	@SqlQuery("SELECT name from Employee where name = ?")
+
+	@SqlQuery("SELECT Name from Employee where Name = ?")
 	@RegisterBeanMapper(Employee.class)
 	Employee getEmployee(String codename);
+	
+	@SqlUpdate("INSERT INTO Employee (Name, Address, Email, NI, accountNumber, sortCode, startingSalary)" +
+	"values (?, ?, ?, ?, ?, ?, ?)")
+	@RegisterBeanMapper(Employee.class)
+	void insertEmployee(String name, String address, String email, String NI, String bankAccount, 
+			float startingSalary);
+	
+		 
 }
